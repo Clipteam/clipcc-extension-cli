01b8f6e7b15e18cf8d4dd86ee483cba3fcec7363
Alex Cui <cuizhihui030925@outlook.com>
2021-08-21 10:30:20 +0800

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+# ClipCC Extension CLI
+
+## Usage
+
+## Build by Your Own
+
+### Requirements
+
+Yarn 3
+
+```bash
+yarn set version berry
+yarn set version 3.0.0-rc.2
+```
--- cli.js
+++ cli.js
@@ -1,49 +1,144 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+const { spawn } = require('child_process');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const mergeUtil = require('merge-util');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
 
-async function copyTemplate(dstPath) {
-    const srcPath = path.join(path.dirname(__filename), 'template');
-    await eachFile(srcPath, function (filePath, isFile) {
-        if (!isFile) {
-            if (!fs.existsSync(path.join(dstPath, filePath))) {
-                fs.mkdirSync(path.join(dstPath, filePath));
-            }
-        }
-        else {
-            console.log(`Copying: ${path.join(dstPath, filePath)}`);
-            const data = fs.readFileSync(path.join(srcPath, filePath));
-            fs.writeFileSync(path.join(dstPath, filePath), data);
-        }
+const { copyTemplate } = require('./util');
+
+const dependency = {
+    plain: [ 'clipcc-extension' ]
+};
+
+const devDependency = {
+    plain: [ 'mkdirp', 'rimraf' ],
+    webpack: [ 'webpack', 'webpack-cli', 'copy-webpack-plugin', 'zip-webpack-plugin' ]
+};
+
+const cmdline = {
+    npm: [ 'npm install --save %s', 'npm install --save-dev %s' ],
+    yarn: [ 'yarn add %s', 'yarn add -D %s' ]
+}
+
+const scripts = {
+    plain: {
+        "build": "rimraf ./build && mkdirp build && rimraf ./dist && mkdirp dist && node build.js",
+        "build:dist": "NODE_ENV=production yarn run build"
+    },
+    webpack: {
+        "build": "rimraf ./build && mkdirp build && rimraf ./dist && mkdirp dist && webpack --colors --bail"
+    }
+}
+
+function runCmd(str) {
+    process.stdout.write(chalk.cyan(`\$ ${str}\n`));
+    const [ cmd, ...arg ] = str.split(' ').filter(v => v.length);
+    const sp = spawn(cmd, arg, { encoding: 'utf-8', stdio: 'inherit' });
+    return new Promise((resolve, _) => {
+        sp.on('close', code => resolve(code));
     });
 }
 
-async function eachFile(srcPath, callback, retPath = '') {
-    fs.readdir(srcPath, (err, files) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        files.forEach(filename => {
-            const filePath = path.join(srcPath, filename);
-            fs.stat(filePath, (err, stat) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                if (stat.isFile()) {
-                    callback(path.join(retPath, filename), true);
-                }
-                else if (stat.isDirectory()) {
-                    callback(path.join(retPath, filename), false);
-                    eachFile(filePath, callback, path.join(retPath, filename));
-                }
-            });
-        });
-    });
+function initGit() {
+    return runCmd('git init');
+}
+
+function convertIdToPacakgeName(id) {
+    return 'clipcc-extension-' + id.replace('.', '-');
+}
+
+function convertAuthor(author) {
+    return author.includes(',') ? author.split(',').map(v => v.trim()) : author;
+}
+
+function createPackage(types, meta, root) {
+    let script = scripts.plain;
+    for (const type of types) {
+        script = mergeUtil(script, scripts[type]);
+    }
+    const pkgInfo = {
+        name: convertIdToPacakgeName(meta.id),
+        version: meta.version,
+        author: convertAuthor(meta.author),
+        scripts: script,
+    };
+    const info = {
+        id: meta.id,
+        author: pkgInfo.author,
+        version: meta.version,
+        icon: 'assets/icon.jpg',
+        inset_icon: 'assets/inset_icon.svg',
+        api: 1
+    };
+    return Promise.all([
+        fs.writeFile(path.join(root, 'package.json'), JSON.stringify(pkgInfo, null, 4)),
+        fs.writeFile(path.join(root, 'info.json'), JSON.stringify(info, null, 4))
+    ]);
+}
+
+async function installDependency(pkg, types) {
+    const dep = dependency.plain;
+    const dev = devDependency.plain;
+    for (const type of types) {
+        if (dependency.hasOwnProperty(type)) dep.push(...dependency[type]);
+        if (devDependency.hasOwnProperty(type)) dev.push(...devDependency[type]);
+    }
+    return runCmd(util.format(cmdline[pkg][0], dep.join(' ')))
+        .then(_ => runCmd(util.format(cmdline[pkg][1], dev.join(' '))));
+}
+
+async function main() {
+    process.stdout.write('Welcome to use clipcc-extension-cli!\n');
+    const packageMeta = await inquirer.prompt([{
+        type: 'input',
+        name: 'id',
+        message: 'Extension ID:',
+        validate: v => /^([a-z0-9_]+\.)+[a-z0-9_]+$/.test(v) ? true : 'Unvalid ID.'
+    }, {
+        type: 'input',
+        name: 'name',
+        message: 'Name:'
+    }, {
+        type: 'input',
+        name: 'description',
+        message: 'Description:'
+    }, {
+        type: 'input',
+        name: 'version',
+        message: 'Version:'
+    }, {
+        type: 'input',
+        name: 'author',
+        message: 'Author:'
+    }]);
+    const { lang, pkg, bundler, git } = await inquirer.prompt([{
+        type: 'list',
+        name: 'lang',
+        message: 'Choose your development language:',
+        choices: [ 'JavaScript' /*, 'TypeScript'*/ ]
+    }, {
+        type: 'list',
+        name: 'pkg',
+        message: 'Choose your package manager:',
+        choices: [ 'npm', 'yarn' /*, 'berry'*/ ]
+    }, {
+        type: 'list',
+        name: 'bundler',
+        message: 'Choose your bundler:',
+        choices: [ 'webpack' /*, 'snowpack'*/ ]
+    }, {
+        type: 'confirm',
+        name: 'git',
+        message: 'Use git?'
+    }]);
+    if (git) await initGit();
+    await createPackage([ bundler ], packageMeta, '.');
+    await installDependency(pkg, [ bundler ]);
 }
 
 const argv = yargs(hideBin(process.argv))
@@ -90,5 +185,7 @@ if (argv.generate) {
     copyTemplate(cwd);
 }
 else {
-    console.log('Unknown command.');
+    main();
 }
+
+
